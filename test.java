33b918fead9e7740755954a4e090394f36235b11
lds500016 <273482144@qq.com>
2018-12-07 11:49:06 +0800

    test.java1.0.1

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class Test{
   public static viod main(String []args){
-    System.out.println("Hello Wrold!!!");
+   	Scanner input = new Scanner(System.in);
+	System.out.println("input first number:");
+	int a=input.nextInt();
+	System.out.println("input second number:");
+	int b=input.nextInt();
+	System.out.println(a+"+"+b+"="+(a+b));
   }
 }
+
